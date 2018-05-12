80bfb7a9da82c0ba148b07c522ef7357c7abcbcc
boolboost <bool.boost@gmail.com>
2018-05-12 20:47:55 +0500

    Update build.sh

--- Drupal 8/docker-compose/build.sh
+++ Drupal 8/docker-compose/build.sh
@@ -10,4 +10,8 @@ docker-compose up -d
 sudo setfacl -dR -m u:$(whoami):rwX -m u:82:rwX -m u:100:rX .
 sudo setfacl -R -m u:$(whoami):rwX -m u:82:rwX -m u:100:rX .
 
+git clone https://github.com/drupal-composer/drupal-project.git
+cp -R drupal-project/* .
+rm -rf drupal-project/
+
 exit 0
